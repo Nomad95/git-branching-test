9b290bf5fa81ff16ef2a824088617c2f91a511c6
Igor Kopeć <igokop99@gmail.com>
2018-07-05 22:23:07 +0200

    ebe

--- src/one/Cat.java
+++ src/one/Cat.java
@@ -3,5 +3,10 @@ package one;
 public class Cat {
     public static void meow() {
         System.out.println("meow meow i am a cat");
+        System.out.println("mrau");
+    }
+
+    public void ebe() {
+
     }
 }
